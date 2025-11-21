e070ae7250931f7157f2908f47f1d5715245120e
kilean-lucas <57876313+kilean-lucas@users.noreply.github.com>
2025-11-21 15:53:36 +0000

    Deploying to gh-pages from @ kilean-lucas/kilean-lucas.github.io@f6253b532d0dd16959e0d9dc330060ad74bac6a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,9 +429,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-microbially-influenced-corrosion",
+          title: 'Microbially Influenced Corrosion',
+          description: "The project that I worked on for my undergraduate research",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
